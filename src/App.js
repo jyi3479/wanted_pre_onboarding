1df015f75e556ce924e36437614836832f2e663a
jyi3479 <jyi3479@naver.com>
2022-04-24 17:09:15 +0900

    input 완성

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import Dropdown from "./components/Dropdown";
 import Input from "./components/Input";
 import Slider from "./components/Slider";
@@ -6,18 +7,44 @@ import Toggle from "./components/Toggle";
 
 function App() {
   return (
-    <div className="App">
-      <Toggle />
+    <Container>
+      <Box>
+        <Title>Toggle</Title>
+        <Toggle />
+      </Box>
+      <Box>
+        <Title>Tap</Title>
+        <Tap />
+      </Box>
+      <Box>
+        <Title>Slider</Title>
+        {/* <Slider /> */}
+      </Box>
 
-      <Tap />
-
-      {/* <Slider /> */}
-
-      <Input />
-      <hr />
-      <Dropdown />
-    </div>
+      <Box>
+        <Title>Input</Title>
+        <Input />
+      </Box>
+      <Box>
+        <Title>Dropdown</Title>
+        <Dropdown />
+      </Box>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Box = styled.div`
+  margin: 20px;
+`;
+
+const Title = styled.p`
+  text-align: center;
+`;
 
 export default App;
--- src/components/Input.js
+++ src/components/Input.js
@@ -2,40 +2,59 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { ReactComponent as Check } from "../image/check.svg";
 import { ReactComponent as Visible } from "../image/visible.svg";
+import { ReactComponent as Invisible } from "../image/noneVisible.svg";
 
 const Input = () => {
-  const [isPrivate, setIsPrivate] = useState(false);
+  const [isEmail, setIsEmail] = useState(false);
+  const [isError, setIsError] = useState(true);
+  const [isPrivate, setIsPrivate] = useState(true);
   const clickedPrivate = () => {
     setIsPrivate(!isPrivate);
   };
 
-  const checkedEmail = (e) => {
-    var regExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+  const checkedEmail = (email) => {
+    const regExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
     // 형식에 맞는 경우 true 리턴
-    console.log("이메일 유효성 검사 :: ", regExp.test(e.target.value));
+
+    return regExp.test(email);
+  };
+
+  const blurEmailChecked = (e) => {
+    setIsError(checkedEmail(e.target.value));
+    if (!e.target.value) {
+      setIsError(true);
+    }
+  };
+
+  const changeEmailChecked = (e) => {
+    setIsEmail(checkedEmail(e.target.value));
   };
 
   return (
     <>
       <InputBox>
         <Label>E-mail</Label>
-        <InputField placeholder="E-mail" onBlur={checkedEmail} />
-        <IconBox>
+        <InputField placeholder="E-mail" onBlur={blurEmailChecked} onChange={changeEmailChecked}></InputField>
+        <IconBox isEmail={isEmail}>
           <Check />
         </IconBox>
+        <Warning isError={isError}>Invalid e-mail address.</Warning>
       </InputBox>
+
       <InputBox>
         <Label>password</Label>
         <InputField placeholder="password" type={isPrivate ? "password" : ""} />
-        <IconBox>
-          <Visible className="visible" onClick={clickedPrivate} />
-        </IconBox>
+        <IconBox onClick={clickedPrivate}>{isPrivate ? <Invisible className="invisible" /> : <Visible className="visible" />}</IconBox>
       </InputBox>
     </>
   );
 };
 
-const InputBox = styled.div``;
+const InputBox = styled.div`
+  position: relative;
+
+  margin-bottom: 20px;
+`;
 
 const Label = styled.p`
   font-size: 10px;
@@ -44,8 +63,6 @@ const Label = styled.p`
 `;
 
 const InputField = styled.input`
-  //position: absolute;
-
   width: 230px;
   height: 25px;
 
@@ -61,13 +78,32 @@ const InputField = styled.input`
   }
 `;
 
+const Warning = styled.p`
+  position: absolute;
+  bottom: -23px;
+
+  font-size: 10px;
+  ${(props) => (props.isError ? `display : none;` : `color: red;`)}
+`;
+
 const IconBox = styled.div`
+  position: absolute;
+  right: 5px;
+  bottom: 0px;
+
   svg {
-    fill: #ccc;
+    width: 15px;
+    ${(props) => (props.isEmail ? `fill: #008299;` : `fill: #ccc;`)}
+  }
+
+  .invisible {
+    fill: #000000;
   }
   .visible {
-    cursor: pointer;
+    fill: #008299;
   }
+
+  cursor: pointer;
 `;
 
 export default Input;
